82e4c599fb5ce790494421873da142356c074079
minguotan <minguotan@gmail.com>
2022-03-17 07:40:50 +0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -23,7 +23,7 @@
                break;
                case 1 :
                document.getElementById("num").src=
-               "https://raw.githubusercontent.com/minguotan/img/main/102.jpg";
+               "https://www.jianguoyun.com/c/tblv2/wyEEQ0jbwCXpm8nDEN-9oHcpVqYyuFpfSwGP3Lh6mp087izqTo6SUnxfmKYjqpTOTrIxtraz/-rSJVcF6-1bOF8AH6-9haw/l";
                break;
                case 2 :
                document.getElementById("num").src=
